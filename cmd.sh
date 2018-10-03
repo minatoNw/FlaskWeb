7f7634843aa3c7d0dd30647385578946c70387a4
Minato <minatonetwork@gmail.com>
2018-10-04 00:10:57 +0900

    change name

--- cmd.sh
+++ cmd.sh
@@ -3,9 +3,9 @@ set -e
 
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
-  exec python3 /app/identidock.py
+  exec python3 /app/flaskweb.py
 else
   echo "Running Production Server"
-  exec python3 /app/identidock.py 
+  exec python3 /app/flaskweb.py 
              
 fi
